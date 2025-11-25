cca76125318c3db509b027a46be9a7beb8a540a6
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-11-24 21:43:46 -0500

    Create LifeTogetherAdmin_Changelogs.js

---
+++ LifeTogetherAdmin_Changelogs.js
@@ -0,0 +1,267 @@
+Nov 24, 2025 - V7.0.8
+• Fixed all current dragging issues.
+• Added 'lockhome' command.
+• Added 'unlockhome' command.
+• Added 'lockplrhome' command.
+• Added 'unlockplrhome' command.
+• Added 'autolockhome' command.
+• Added 'unautolockhome' command.
+• Added 'autolockplrhome' command.
+• Added 'unautolockplrhome' command.
+
+Nov 24, 2025 - V6.9.9
+• Solved an issue with 'lagreducer' when turning it on making Life Together's code error out, causing issues when disabling 'lagreducer'.
+
+Nov 24, 2025 - V6.9.8
+• Fully fixed 'lagreducer' command now works a different way than setting users settings.
+
+Nov 24, 2025 - V6.9.7
+• Fully fixed Command Helper GUI erroring when trying to close it.
+
+Nov 24, 2025 - V6.9.6
+• Fixed rules agreement and it's GUI to allow you to actually rejoin to agree.
+
+Nov 24, 2025 - V6.9.5
+• Attempted to fix 'lagreducer' command errors (multi-level support).
+
+Nov 24, 2025 - V6.9.4
+• Fixed 'lagreducer' command saying it's enabled, when it's not.
+
+Nov 24, 2025 - V6.9.3
+• Fixed 'lagreducer' command not showing question notifications on mobile.
+
+Nov 24, 2025 - V6.9.2
+• Fixed 'AntiCarFling' putting your Vehicle underground (will let you spawn your Vehicle normally).
+
+Nov 24, 2025 - V6.9.1
+• Fixed Billboard/Title system still saying users are online and giving them a Billboard (when they're not).
+
+Nov 24, 2025 - V6.9.0
+• Fixed Fly-1 command not working.
+• Fixed Fly-2 command not working.
+• Improved anti-logging/warnings system (also clears errors).
+
+Nov 22, 2025 - V6.8.7
+• Fixed some commands not working.
+• Added universal holiday detection system, which will tell you on the bottom what holiday it is.
+
+Nov 22, 2025 - V6.8.5
+• Attempted to fix billboard/title system to properly update users (is still being worked on).
+
+Nov 22, 2025 - V6.8.4
+• Updated internal RemoteEvent/RemoteFunction system to respond more quickly to commands.
+
+Nov 22, 2025 - V6.8.2
+• Fixed 'ban' command not working.
+• Fixed 'kick' command not working.
+
+Nov 22, 2025 - V6.8.0
+• Added new priv server commands.
+• Added 'ban Player' command (bans in-game + offline players) from ur priv server.
+• Added 'unban Player' (online + offline) command from priv servers.
+• Added 'kick Player' command (only in-game).
+
+Nov 20, 2025 - V6.7.6
+• Fixed the 'Rules GUI/Menu'.
+
+Nov 20, 2025 - V6.7.5
+• Added a 'Rules GUI/Menu', making sure you acknowledge that you can be banned if you abuse, especially against other Flames Hub users.
+
+Nov 20, 2025 - V6.7.4
+• Fixed 'lagreducer' command not showing as a command/not working.
+• Made it a toggle-able command.
+
+Nov 20, 2025 - V6.7.2
+• Added 'lagreducer' command.
+• Fixed not being able to see your own title.
+• My bad, I was eating, so this update came after I was done.
+
+Nov 20, 2025 - V6.7.0
+• Fixed 'spin' command not being set correctly for character size.
+
+Nov 20, 2025 - V6.6.9
+• Added Vehicle spoofer (when starting the script up) will spawn like 5-10 different cars and spoof them for you.
+
+Nov 20, 2025 - V6.6.8
+• Fixed title system not working (down for update).
+• Fixed Command Helper GUI not closing properly sometimes.
+• Fixed command helper background keeping color stuck with RGB.
+• Fixed some small character issues.
+
+Nov 19, 2025 - V6.6.4
+• Re-working the title system, so if it's not been showing, that's why.
+
+Nov 19, 2025 - V6.6.3
+• I apologize for the down-time, I missed some things, and I've been trying to fix Mobile + I'm working on a script for a new game.
+
+Nov 17, 2025 - V6.6.0
+• Improved the Feedback system, now can give you auto-responses depending on what you enter.
+
+Nov 17, 2025 - V6.5.8
+• Hopefully fixed the CommandBar GUI size issue on Mobile devices.
+
+Nov 17, 2025 - V6.5.7
+• Fixed 'Humanoid must be in the DataModel to play Emotes' breaking emotes when respawning.
+
+Nov 17, 2025 - V6.5.6
+• Added 'configgui' as a command, so you can set your auto-load Configuration commands.
+
+Nov 17, 2025 - V6.5.5
+• Fixed WalkFling-V2 not stopping correctly, making some characters sort of 'bug out'.
+
+Nov 17, 2025 - V6.5.4
+• Moved the CmdBar so Roblox can stop interfering with the CmdBar GUI (breaking side Phone button).
+
+Nov 17, 2025 - V6.5.3
+• Hopefully fixed the kicking issues in the script with Mobile especially (changed stuff).
+
+Nov 17, 2025 - V6.5.2
+• Removed RateLimiter bypass for Mobile pending investigation into the Phone disappearing.
+• Changed some code to hopefully maybe resolve the Phone disappearing.
+
+Nov 16, 2025 - V6.5.0
+• Added 'namespam' command (changes your name extremely fast).
+• Added 'unnamespam' command.
+• Temporarily brung back 'CmdBar' for mobile to see if it works.
+
+Nov 16, 2025 - V6.4.7
+• Made globals (getgenv() environment) system more stable.
+• Improved Character/Character respawn system/caching.
+
+Nov 16, 2025 - V6.4.5
+• Made UIs on mobile devices a bit smaller for clarity.
+
+Nov 14, 2025 - V6.4.4
+• Fixed 'viewcar' command not turning off automatically when Vehicle is destroyed/disappears.
+• Added 'unviewcar' command.
+
+Nov 14, 2025 - V6.4.2
+• Added 'viewcar' command, which makes you view your whole Vehicle.
+
+Nov 14, 2025 - V6.4.1
+• Fixed 'copyanim' command not working properly.
+
+Nov 14, 2025 - V6.4.0
+• Fixed 'spin' command not turning off properly.
+• Added 'spinspeed' command (changes spin speed).
+
+Nov 14, 2025 - V6.3.8
+• Made emote logic faster to play, and you still can move while emoting.
+
+Nov 14, 2025 - V6.3.7
+• Added 'copyemote' command, allowing you to copy a players emote/anim.
+• Added 'spin' command (with speed).
+• Added 'unspin' command.
+• Added 'mutetools' command.
+• Added 'unmutetools' command.
+
+Nov 13, 2025 - V6.3.3
+• Added auto-updater (you don't need to rejoin anymore!).
+• Fixed get_root functionality (just incase the game switches stuff up).
+
+Nov 13, 2025 - V6.3.2
+• Made the Title system be able to check if the user is actually running the Admin script first before giving them a Title.
+
+Nov 13, 2025 - V6.3.1.
+• Fixed 'antihouseban' command.
+• Fixed 'unantiban' command.
+• Added auto-updater (you don't need to rejoin anymore).
+• Fixed internal commands system some more (polished).
+
+Nov 13, 2025 - V6.2.7
+• Made the Title system a true Heartbeat system (so it updates users properly).
+
+Nov 13, 2025 - V6.2.5
+• Removed 'cmdbar' for mobile users (was causing issues, will be back soon!).
+
+Nov 13, 2025 - V6.2.4
+• I apologize for the downtime, I had to put the code somewhere else, it was too much for mobile users.
+
+Nov 13, 2025 - V6.2.0
+• Fixed the root cause of the mobile Sidebar Phone disappearance issue (no mobile CmdBar toggle button).
+
+Nov 13, 2025 - V6.1.9
+• Actually fixed Phone Sidebar button disappearing (errors/issues resolved).
+• Solved some more positioning issues with dragging GUIs on mobile.
+• Added notifications to the 'rgbtool' command.
+
+Nov 13, 2025 - V6.1.6
+• Fixed an error that was making the Sidebar Phone button disappear.
+• Fixed mobile positioning with some GUIs and closing GUIs.
+• Fixed dragging on mobile for 'Flames | Settings'.
+
+Nov 12, 2025 - V6.1.3
+• Hopefully fixed the Sidebar Phone button disappearing.
+• Fixed color wheel breaking GUI position sometimes.
+
+Nov 12, 2025 - V6.1.1
+• Fixed the color on the Title system.
+• Made RGB GUIs/Menus a little faster.
+
+Nov 12, 2025 - V6.0.9
+• Made RGB Menus/GUIs fully toggle-able through the Flames | Settings.
+• Fixed the positioning for Flames | Settings.
+• Added Title Background Color changer (saves too!).
+• Made RGB speed a little quicker.
+• More updates soon...
+
+Nov 12, 2025 - V6.0.5
+• Made Prefix/Credits Label RGB/Rainbow.
+• Made Commands Menu GUI RGB/Rainbow.
+• Made Changelogs GUI RGB/Rainbow.
+• CmdBar is also RGB/Rainbow.
+• Please do not hesitate to report issues to me directly or through my feedback command, and enjoy!
+• Major updates soon...
+
+Nov 12, 2025 - V6.0.0
+• Fixed title system to update dynamically/automatically, welcome to V6 brothers and sisters!
+
+Nov 11, 2025 - V5.9.9
+• Added 'privserver' command, that'll notify you who the Private Server owner is (if you're in one).
+
+Nov 11, 2025 - V5.9.8
+• Almost in V6!!! who is ready?!
+• Added settings system for the titles (top right).
+• Added custom title setting system, allowing you to set what ever title you want, unfiltered
+• Added new emote 'tuff'.
+
+Nov 11, 2025 - V5.9.5
+• Fully fixed Sidebar Phone button toggle (fully replicated now, will not disappear).
+• Fixed top right 'Toggle Flames Hub Titles' button being too big for mobile.
+• Please use my 'feedback' command to report any further issues as well, do not hesitate to do so!
+
+Nov 11, 2025 - V5.9.3
+• Fixed the Sidebar Phone toggle system disappearing randomly (cloned it as well).
+
+Nov 11, 2025 - V5.9.2
+• Shout-out to @IIIIX27gotdeleted, when having the Titles off, they would still show when someone else joined with the script executed.
+• Solved rare-case issue where Titles we're not loading/showing automatically.
+
+Nov 11, 2025 - V5.8.9
+• Fixed title system completely, actually shows users that are using the script.
+• Added 'Toggle Titles' button (to hide/show titles).
+
+Nov 10, 2025 - V5.8.7
+• Fixed 'rgbtool' command not finding tools placed in PlacedModels (when placing them down on the floor).
+
+Nov 10, 2025 - V5.8.6
+• Fixed title/billboard system, every user of Flames Hub will have a client title above their heads.
+
+Nov 10, 2025 - V5.8.4
+• Fixed NoClip not disabling properly, but somehow enabling properly lol.
+
+Nov 10, 2025 - V5.8.3
+• Shout-out to @Gotbannedburito1 for telling me the positioning was wrong on the Changelogs GUI.
+
+Nov 10, 2025 - V5.8.2
+• Added 'carstats' command, allowing you to see other Vehicles players own, and spawn them with their color, settings, etc.
+
+Nov 10, 2025 - V5.8.0
+• Added 'V2' WalkFling to fix the issues with the old WalkFling.
+• Added 'changelogs' command so you can see what was added/removed/changed!
+• Fixed some mobile dragging issues.
+
+Nov 9, 2025 - V5.7.9
+• Added 'msggui' that allows you to send messages through the in-game Phone system.
+• Added RateLimiter bypass, and more.
+• Fixed emoting issues, dragging issues, improved 'FlashTime' command, and more.
