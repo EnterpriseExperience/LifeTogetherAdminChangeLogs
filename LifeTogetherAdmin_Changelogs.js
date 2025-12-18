d3e785ccd48a400bbd66ed9083e2a61cf690cd89
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2025-12-18 04:12:45 -0500

    Update LifeTogetherAdmin_Changelogs.js

--- LifeTogetherAdmin_Changelogs.js
+++ LifeTogetherAdmin_Changelogs.js
@@ -1,3 +1,7 @@
+Dec 18th, 2025 - V7.3.5
+• Fixed update checking and updating, I forgot to set it dynamically, it was static lmfao.
+• Added actual "infyield" command (or you can do the command "loadiy" or "iy", now Infinite Premium is the command "infpremium" basically).
+
 Dec 17th, 2025 - V7.3.3
 • I know I don't update this script anymore, but I'll maybe come back to it one day.
 • Fixed script version issues and the script reloading on it's own randomly.
