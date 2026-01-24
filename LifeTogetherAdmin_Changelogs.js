5571d27454576bf029eb0c505adcd77d9c9723d3
FlamesHub_Official <165463731+EnterpriseExperience@users.noreply.github.com>
2026-01-24 11:06:20 -0500

    Update LifeTogetherAdmin_Changelogs.js

--- LifeTogetherAdmin_Changelogs.js
+++ LifeTogetherAdmin_Changelogs.js
@@ -1,4 +1,4 @@
-January 24th, 2026 - No Change
+January 24th, 2026 - V7.9.7
 • Shoutout to @jdkdjfjjfjff0 (on Roblox) for telling me the Vehicle Stats GUI was broken
 • If you want your idea in the script, or need to report a bug/issue send them through with the 'feedback' command.
 
